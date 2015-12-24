ad8dc262e473198fb29c8f53ebaef529b4739135
elico <eliezer@ngtech.co.il>
2015-12-24 10:51:36 +0200

    Adding a flag to enabled or disable the logging

--- httpLogger.go
+++ httpLogger.go
@@ -27,20 +27,26 @@ func (w *statusWriter) Write(b []byte) (int, error) {
 }
 
 // WriteLog Logs the Http Status for a request into fileHandler and returns a httphandler function which is a wrapper to log the requests.
-func WriteLog(handle http.Handler, fileHandler *os.File) http.HandlerFunc {
-	logger := log.New(fileHandler, "", 0)
-	return func(w http.ResponseWriter, request *http.Request) {
-		start := time.Now()
-		writer := statusWriter{w, 0, 0}
-		handle.ServeHTTP(&writer, request)
-		end := time.Now()
-		latency := end.Sub(start)
-		statusCode := writer.status
-		length := writer.length
-		if request.URL.RawQuery != "" {
-			logger.Printf("%v %s %s \"%s %s%s%s %s\" %d %d \"%s\" %v", end.Format("2006/01/02 15:04:05"), request.Host, request.RemoteAddr, request.Method, request.URL.Path, "?", request.URL.RawQuery, request.Proto, statusCode, length, request.Header.Get("User-Agent"), latency)
-		} else {
-			logger.Printf("%v %s %s \"%s %s %s\" %d %d \"%s\" %v", end.Format("2006/01/02 15:04:05"), request.Host, request.RemoteAddr, request.Method, request.URL.Path, request.Proto, statusCode, length, request.Header.Get("User-Agent"), latency)
+func WriteLog(handle http.Handler, fileHandler *os.File, *enabled bool) http.HandlerFunc {
+	if enabled {
+		logger := log.New(fileHandler, "", 0)
+		return func(w http.ResponseWriter, request *http.Request) {
+			start := time.Now()
+			writer := statusWriter{w, 0, 0}
+			handle.ServeHTTP(&writer, request)
+			end := time.Now()
+			latency := end.Sub(start)
+			statusCode := writer.status
+			length := writer.length
+			if request.URL.RawQuery != "" {
+				logger.Printf("%v %s %s \"%s %s%s%s %s\" %d %d \"%s\" %v", end.Format("2006/01/02 15:04:05"), request.Host, request.RemoteAddr, request.Method, request.URL.Path, "?", request.URL.RawQuery, request.Proto, statusCode, length, request.Header.Get("User-Agent"), latency)
+			} else {
+				logger.Printf("%v %s %s \"%s %s %s\" %d %d \"%s\" %v", end.Format("2006/01/02 15:04:05"), request.Host, request.RemoteAddr, request.Method, request.URL.Path, request.Proto, statusCode, length, request.Header.Get("User-Agent"), latency)
+			}
 		}
+	} else {
+		return func(w http.ResponseWriter, request *http.Request) {
+			handle.ServeHTTP(&writer, request)
+		}	
 	}
 }
